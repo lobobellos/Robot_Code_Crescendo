f665fb6d03e4bdfc7f614002a87f02e3e2419a88
= <918780@dpsk12.net>
2024-02-02 21:53:34 -0700

    add toggle void

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ElevatorConstants;
@@ -14,20 +15,34 @@ public class Elevator extends SubsystemBase {
       ElevatorConstants.BottomMotorID,
       CANSparkMax.MotorType.kBrushless);
 
+  private double kSpeedPercent = 0;
+
   public Elevator() {
     intakeMotorTop.setInverted(ElevatorConstants.kTopInverted);
     intakeMotorBottom.setInverted(ElevatorConstants.kBottomInverted);
 
     this.setDefaultCommand(new RunCommand(this::run, this));
+
+  }
+
+  public void toggleEnabled() {
+    if(kSpeedPercent == 0) {
+      kSpeedPercent = ElevatorConstants.kSpeedPercent;
+    } else {
+      kSpeedPercent = 0;
+    }
   }
 
   public void run() {
-    intakeMotorTop.set(ElevatorConstants.kSpeedPercent);
-    intakeMotorBottom.set(ElevatorConstants.kSpeedPercent);
+    intakeMotorTop.set(kSpeedPercent);
+    intakeMotorBottom.set(kSpeedPercent);
   }
 
   public void stop() {
-    intakeMotorTop.set(0);
-    intakeMotorBottom.set(0);
+    kSpeedPercent = 0;
+  }
+
+  public void setSpeed(double target){
+    kSpeedPercent = MathUtil.clamp(target,0,1);
   }
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
@@ -14,20 +15,33 @@ public class Intake extends SubsystemBase {
       IntakeConstants.BottomMotorID,
       CANSparkMax.MotorType.kBrushless);
 
+  private double kSpeedPercent = 0;
+
   public Intake() {
     intakeMotorTop.setInverted(IntakeConstants.kTopInverted);
     intakeMotorBottom.setInverted(IntakeConstants.kBottomInverted);
-
     this.setDefaultCommand(new RunCommand(this::run, this));
   }
 
+  public void toggleEnabled() {
+    if(kSpeedPercent == 0) {
+      kSpeedPercent = IntakeConstants.kSpeedPercent;
+    } else {
+      kSpeedPercent = 0;
+    }
+  }
+
   public void run() {
-    intakeMotorTop.set(IntakeConstants.kSpeedPercent);
-    intakeMotorBottom.set(IntakeConstants.kSpeedPercent);
+    intakeMotorTop.set(kSpeedPercent);
+    intakeMotorBottom.set(kSpeedPercent);
   }
 
   public void stop() {
     intakeMotorTop.set(0);
     intakeMotorBottom.set(0);
+  }
+
+  public void setSpeed(double target){
+    kSpeedPercent = MathUtil.clamp(target,0,1);
   }
 }
