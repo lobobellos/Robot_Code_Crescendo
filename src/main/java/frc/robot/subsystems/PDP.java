6ff866cd0ce568ac3d7b7bcacf300a7511a61507
= <918780@dpsk12.net>
2024-02-02 18:42:00 -0700

    delete redundant code

--- src/main/java/frc/robot/subsystems/PDP.java
+++ src/main/java/frc/robot/subsystems/PDP.java
@@ -4,7 +4,6 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PDP extends SubsystemBase {
@@ -17,12 +16,4 @@ public class PDP extends SubsystemBase {
     tab.addNumber("pdp/voltage", pdp::getVoltage);
     tab.addNumber("pdp/current0", ()->pdp.getCurrent(0));
   }
-  
-  public RunCommand updatePDP(){
-    return new RunCommand(()->{
-      
-    }, this);
-  }
-
-
 }
