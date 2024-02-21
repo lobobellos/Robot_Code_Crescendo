f86c77647de18f614eac6339fec442074b0e7a2f
bakedPotatoLord <jeffy9745@gmail.com>
2024-02-20 17:16:45 -0700

    finalize pneumatics

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -3,24 +3,25 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticHub;
+import edu.wpi.first.wpilibj.PneumaticsControlModule;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.PneumaticsConstants;
 
 public class Pneumatics extends SubsystemBase {
   
-  private PneumaticHub pneumaticHub = new PneumaticHub(PneumaticsConstants.kPneumaticHubPort);
+  private PneumaticsControlModule controller = new PneumaticsControlModule(11);
+  private Compressor compressor = new Compressor(11,PneumaticsModuleType.CTREPCM);
 
-  private Compressor compressor = pneumaticHub.makeCompressor();
-
-  private DoubleSolenoid shooterSolenoid = pneumaticHub.makeDoubleSolenoid(
+  private DoubleSolenoid shooterSolenoid = controller.makeDoubleSolenoid(
     PneumaticsConstants.shooterDoubleSolenoid[0],
     PneumaticsConstants.shooterDoubleSolenoid[1]
   );
 
   public Pneumatics(){
-    disableCompressor();
-    
+    enableCompressor();
+    setShooterPiston(true);
   }
 
   public boolean compressorEnabled(){
@@ -29,26 +30,27 @@ public class Pneumatics extends SubsystemBase {
 
   
   public void enableCompressor(){
-    compressor.enableHybrid(100, 110);
+    //compressor.enableAnalog(70, 110);
+    compressor.enableDigital();
+    System.out.println("enabling compressor");
   }
   
   public void disableCompressor(){
     compressor.disable();
+    System.out.println("disabling compressor");
   }
-
-  
 
   public void setShooterPiston(boolean state){
     shooterSolenoid.set( state ? DoubleSolenoid.Value.kForward : DoubleSolenoid.Value.kReverse );
   }
 
   public void toggleShooterPiston(){
-    shooterSolenoid.toggle  ();
+    shooterSolenoid.toggle();
   }
 
   public void periodic(){
-    SmartDashboard.putBoolean("compressor enabled", compressorEnabled());
-    SmartDashboard.putNumber("PSI", pneumaticHub.getPressure(0));
+    //SmartDashboard.putBoolean("compressor enabled", compressorEnabled());
+    SmartDashboard.putNumber("compressor current", compressor.getCurrent());
   }
 
 }
