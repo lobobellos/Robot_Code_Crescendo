1a45f6b534eb29bbc0c7af74b8745ffd6ede784c
bakedPotatoLord <jeffy9745@gmail.com>
2024-02-27 09:03:28 -0800

    switch to navX

--- src/main/java/frc/robot/subsystems/Gyro.java
+++ src/main/java/frc/robot/subsystems/Gyro.java
@@ -2,35 +2,37 @@ package frc.robot.subsystems;
 
 
 
+import com.kauailabs.navx.frc.AHRS;
+
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.ADIS16470_IMU;
-import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
+import edu.wpi.first.wpilibj.SerialPort;
+
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Gyro extends SubsystemBase {
 
-  public static final ADIS16470_IMU imu = new ADIS16470_IMU();
+  public static final AHRS ahrs = new AHRS(SerialPort.Port.kMXP); 
 
   public Gyro() {
     //recalibrate();
-    addChild("imu", imu);
+    addChild("imu", ahrs);
   }
 
   public InstantCommand zero() {
-    return new InstantCommand(()->imu.reset(), this);
+    return new InstantCommand(ahrs::zeroYaw, this);
   }
 
   public void recalibrate() {
-    imu.calibrate();
+    ahrs.reset();
   }
 
   public Rotation2d getRotation() {
-    return Rotation2d.fromDegrees(imu.getAngle(IMUAxis.kYaw));
+    return ahrs.getRotation2d();
   }
 
   // returns angular velocity in Rotation2d per second
   public Rotation2d getAngularVelocity() {
-    return Rotation2d.fromDegrees(imu.getRate(IMUAxis.kYaw));
+    return Rotation2d.fromDegrees(ahrs.getRawGyroY());
   }
 }
