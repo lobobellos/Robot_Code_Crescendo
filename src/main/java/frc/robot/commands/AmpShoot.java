131f1e6288a71774d2bb0b21be7bf4a362778534
bakedPotatoLord <jeffy9745@gmail.com>
2024-03-04 14:49:25 -0700

    lock drivebase while running

--- src/main/java/frc/robot/commands/AmpShoot.java
+++ src/main/java/frc/robot/commands/AmpShoot.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Pneumatics;
 import frc.robot.subsystems.Shooter;
 
@@ -12,14 +13,16 @@ public class AmpShoot extends ParallelCommandGroup {
 	// sequential
 	// wait for flywheel to be up to speed
 	// shoot note
-	public AmpShoot(Shooter shooter, Pneumatics pneumatics) {
+	public AmpShoot(Shooter shooter, Pneumatics pneumatics, DriveSubsystem drive) {
 		super(
 				Commands.race(
 						shooter.runAmpCommand(),
+						drive.stopCommand(),
 						Commands.sequence(
 								Commands.waitUntil(shooter::atSetpoint),
 								Commands.waitSeconds(1),
 								new SolenoidOneShot(pneumatics),
-								Commands.waitSeconds(0.5))));
+								Commands.waitSeconds(0.5))
+						));
 	}
 }
