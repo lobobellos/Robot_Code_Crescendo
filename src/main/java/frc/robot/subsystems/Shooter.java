d368a22534dc6be96116dd44187bd27b8ac01679
bakedPotatoLord <jeffy9745@gmail.com>
2024-03-04 10:32:54 -0700

    add runcommands and atsetpoint boolean

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -11,12 +12,17 @@ import frc.robot.Constants.ShooterConstants;
 
 public class Shooter extends SubsystemBase {
 
-    CANSparkMax topSparkMax = new CANSparkMax(ShooterConstants.topMotorID, MotorType.kBrushless);
-    CANSparkMax bottomSparkMax = new CANSparkMax(ShooterConstants.bottomMotorID, MotorType.kBrushless);
+    private final CANSparkMax topSparkMax = new CANSparkMax(ShooterConstants.topMotorID, MotorType.kBrushless);
+    private final CANSparkMax bottomSparkMax = new CANSparkMax(ShooterConstants.bottomMotorID, MotorType.kBrushless);
 
-    SparkMaxPIDController topController = topSparkMax.getPIDController();
-    SparkMaxPIDController bottomController = bottomSparkMax.getPIDController();
+    private final SparkMaxPIDController topController = topSparkMax.getPIDController();
+    private final SparkMaxPIDController bottomController = bottomSparkMax.getPIDController();
 
+    private final RelativeEncoder topEncoder = topSparkMax.getEncoder();
+    private final RelativeEncoder bottomEncoder = bottomSparkMax.getEncoder();
+
+    private double topSetpoint;
+    private double bottomSetpoint;
 
     public Shooter() {
         topSparkMax.setIdleMode(IdleMode.kCoast);
@@ -28,7 +34,6 @@ public class Shooter extends SubsystemBase {
         topController.setD(ShooterConstants.FlywheelPIDF.kD,0);
         topController.setFF(ShooterConstants.FlywheelPIDF.kF,0);
 
-
         bottomSparkMax.setIdleMode(IdleMode.kCoast);
         bottomSparkMax.setInverted(ShooterConstants.kBottomInverted);
         bottomSparkMax.setSmartCurrentLimit(ShooterConstants.stallCurentLimit,
@@ -37,11 +42,13 @@ public class Shooter extends SubsystemBase {
     }
 
     private void setSpeeds(double topSpeed, double bottomSpeed) {
+        topSetpoint = topSpeed;
+        bottomSetpoint = bottomSpeed;
         topController.setReference(topSpeed,CANSparkMax.ControlType.kVelocity);
         bottomController.setReference(bottomSpeed,CANSparkMax.ControlType.kVelocity);
     }
 
-    public void run() {
+    public void runAmp() {
         setSpeeds(ShooterConstants.topMotorSpeed,
                 ShooterConstants.bottomMotorSpeed);
     }
@@ -49,5 +56,14 @@ public class Shooter extends SubsystemBase {
     public void stop() {
         topSparkMax.stopMotor();
         bottomSparkMax.stopMotor();
+    }
+
+    public RunCommand runAmpCommand(){
+        return new RunCommand(this::runAmp,this);
+    }
+
+    public boolean atSetpoint(){
+        return Math.abs(topEncoder.getVelocity() - topSetpoint) < ShooterConstants.speedTolerance &&
+            Math.abs(bottomEncoder.getVelocity() - bottomSetpoint) < ShooterConstants.speedTolerance;
     }
 }
