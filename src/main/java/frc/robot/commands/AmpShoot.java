1658aa5d647ab78d3b4df2dad15c5bafbf1261c2
bakedPotatoLord <jeffy9745@gmail.com>
2024-03-04 10:33:48 -0700

    create full ampshoot command group

--- src/main/java/frc/robot/commands/AmpShoot.java
+++ src/main/java/frc/robot/commands/AmpShoot.java
@@ -1,20 +1,24 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.subsystems.Pneumatics;
 import frc.robot.subsystems.Shooter;
 
-public class AmpShoot extends ParallelCommandGroup{
+public class AmpShoot extends ParallelCommandGroup {
 
-    //parallel
-        //riun flywheel
-        //sequential
-            // wait for flywheel to be up to speed
-            // activate solenoid
-            // wait?
-            // toggle solenoid
-    public AmpShoot(Shooter shooter,Pneumatics pneumatics ){
-        
-    }
-
+	// parallel
+	// run flywheel
+	// sequential
+	// wait for flywheel to be up to speed
+	// shoot note
+	public AmpShoot(Shooter shooter, Pneumatics pneumatics) {
+		super(
+				Commands.race(
+						shooter.runAmpCommand(),
+						Commands.sequence(
+								Commands.waitUntil(shooter::atSetpoint),
+								new SolenoidOneShot(pneumatics),
+								Commands.waitSeconds(0.5))));
+	}
 }
