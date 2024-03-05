8443544c4fc33bf8b3fbd723f749f554f1104fcc
bakedPotatoLord <jeffy9745@gmail.com>
2024-03-04 20:12:25 -0700

    make a speaker command group

---
+++ src/main/java/frc/robot/commands/SpeakerShoot.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Pneumatics;
+import frc.robot.subsystems.Shooter;
+
+public class SpeakerShoot extends ParallelCommandGroup {
+
+	// parallel
+	// run flywheel
+	// sequential
+	// wait for flywheel to be up to speed
+	// shoot note
+	public SpeakerShoot(Shooter shooter, Pneumatics pneumatics, DriveSubsystem drive) {
+		super(
+				Commands.race(
+						shooter.runSpeakerCommand(),
+						drive.stopCommand(),
+						Commands.sequence(
+								//Commands.waitUntil(shooter::atSetpoint),
+								Commands.waitSeconds(1),
+								new SolenoidOneShot(pneumatics),
+								Commands.waitSeconds(0.5))
+						));
+	}
+}
