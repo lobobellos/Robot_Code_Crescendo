9023ae5ac0a225a5062259bc3bf9173b5d26fe2e
bakedPotatoLord <jeffy9745@gmail.com>
2024-03-06 15:27:06 -0700

    add timetoShoot constant

--- src/main/java/frc/robot/commands/SpeakerShoot.java
+++ src/main/java/frc/robot/commands/SpeakerShoot.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Pneumatics;
 import frc.robot.subsystems.Shooter;
@@ -20,7 +21,7 @@ public class SpeakerShoot extends ParallelCommandGroup {
 						drive.stopCommand(),
 						Commands.sequence(
 								//Commands.waitUntil(shooter::atSetpoint),
-								Commands.waitSeconds(1),
+								Commands.waitSeconds(ShooterConstants.timeToShoot),
 								new SolenoidOneShot(pneumatics),
 								Commands.waitSeconds(0.5))
 						));
