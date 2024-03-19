73c7ffeb37d263dbbe774c1be249a4650ac8a33a
bakedPotatoLord <jeffy9745@gmail.com>
2024-03-18 19:28:01 -0600

    add arm pivot

--- src/main/java/frc/robot/commands/IntakeElevatorRun.java
+++ src/main/java/frc/robot/commands/IntakeElevatorRun.java
@@ -1,13 +1,19 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.ArmPivotConstants.jointPosition;
+import frc.robot.subsystems.ArmPivot;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
 
-public class IntakeElevatorRun extends ParallelCommandGroup {
-	public IntakeElevatorRun(Intake intake, Elevator elevator) {
+public class IntakeElevatorRun extends SequentialCommandGroup {
+	public IntakeElevatorRun(Intake intake, Elevator elevator, ArmPivot pivot) {
 		super(
-				intake.runCommand(),
-				elevator.runCommand());
+				pivot.setPositionCommand(jointPosition.starting),
+				Commands.parallel(
+						intake.runCommand(),
+						elevator.runCommand()));
+
 	}
 }
