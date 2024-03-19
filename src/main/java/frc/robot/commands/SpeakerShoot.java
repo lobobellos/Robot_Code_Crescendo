881a9cd53297eede51a35faa86678413605205ba
bakedPotatoLord <jeffy9745@gmail.com>
2024-03-18 19:25:24 -0600

    set pivot while shooting

--- src/main/java/frc/robot/commands/SpeakerShoot.java
+++ src/main/java/frc/robot/commands/SpeakerShoot.java
@@ -3,6 +3,8 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Constants.ShooterConstants;
+import frc.robot.Constants.ArmPivotConstants.jointPosition;
+import frc.robot.subsystems.ArmPivot;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Pneumatics;
 import frc.robot.subsystems.Shooter;
@@ -14,16 +16,18 @@ public class SpeakerShoot extends ParallelCommandGroup {
 	// sequential
 	// wait for flywheel to be up to speed
 	// shoot note
-	public SpeakerShoot(Shooter shooter, Pneumatics pneumatics, DriveSubsystem drive) {
+	public SpeakerShoot(Shooter shooter, Pneumatics pneumatics, DriveSubsystem drive, ArmPivot pivot) {
 		super(
 				Commands.race(
 						shooter.runSpeakerCommand(),
 						drive.stopCommand(),
 						Commands.sequence(
-								//Commands.waitUntil(shooter::atSetpoint),
+								// Commands.waitUntil(shooter::atSetpoint),
 								Commands.waitSeconds(ShooterConstants.timeToShoot),
 								new SolenoidOneShot(pneumatics),
-								Commands.waitSeconds(0.5))
-						));
+								Commands.waitSeconds(0.5))),
+				pivot.setPositionCommand(jointPosition.high)
+
+		);
 	}
 }
